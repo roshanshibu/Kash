9bab98b834f5524947114095197bfd39d7c5a74b
roshan <roshanshibu97@gmail.com>
2022-03-19 17:57:03 +0530

    account table creation

--- base.py
+++ base.py
@@ -1,5 +1,35 @@
 import sqlite3 as sql
+import logging
 
+#logging options
+log_to_file = 0
+log_format = '%(asctime)s | %(levelname)s | %(funcName)s() :: %(message)s'
+if (log_to_file != 0):
+	logging.basicConfig(filename='kash.log', encoding='utf-8', level=logging.DEBUG, format=log_format, datefmt='%m/%d/%Y %I:%M:%S %p')
+else:
+	logging.basicConfig(format=log_format, level=logging.DEBUG, datefmt='%m/%d/%Y %I:%M:%S %p')
+
+
+def execute(conn, sql_command, params = None):
+	""" execute a given sql statement
+	conn -> Connection object
+	sql_command -> any sql statement
+	params -> a tuple containg variables that can be plugged into the sql_command
+	"""
+	logging.debug("sql_command = ["+sql_command+"]")
+	try:
+		c = conn.cursor()
+		if params is None:
+			c.execute(sql_command)
+		else:
+			c.execute(sql_command, params)
+		return (True)
+	except sql.Error as e:
+		logging.error(e)
+		return (False)
+	except:
+		logging.error("Unexpected Error!")
+		return (False)
 
 #create and connect to the db "kash" if not already present
 database = "kash.db"
@@ -10,6 +40,30 @@ except sql.Error as e:
 	print ("Exception while creating/connecting to database file [",database,"]:\n{",e,"}")
 
 #develop funstions for each functionality
+
 '''
 1) create an account and store its details
 '''
+#create table account and account_type if not already present
+sql_create_account_table = """ CREATE TABLE IF NOT EXISTS account (
+									id INTEGER PRIMARY KEY AUTOINCREMENT,
+									name TEXT,
+									type_id INT,
+									balance INT,
+									created INT,
+									FOREIGN KEY (type_id) REFERENCES account_type (id)									
+								); """
+sql_create_account_type_table = """ CREATE TABLE IF NOT EXISTS account_type (
+									id INTEGER PRIMARY KEY AUTOINCREMENT,
+									name TEXT,
+									logo_id TEXT
+								); """
+
+if (not execute(conn, sql_create_account_type_table)):
+	logging.error ("Failed to create table account_type. Exiting...")
+	exit(0)
+
+if (not execute(conn, sql_create_account_table)):
+	logging.error ("Failed to create table account. Exiting...")
+	exit(0)
+	
